324f1467aed3684603ce434a8911165d0372b7ea
Maru40 <umeda.4072@gmail.com>
2023-05-05 00:37:39 +0900

    StateNode_NormalのUpdateなどの空実装

--- AITester/GameSources/Maruyama/Enemy/StateNodes/StateNode_Normal.cpp
+++ AITester/GameSources/Maruyama/Enemy/StateNodes/StateNode_Normal.cpp
@@ -1,9 +1,29 @@
 #include "StateNode_Normal.h"
 
+#include "Maruyama/Enemy/EnemyBase.h"
+
 namespace Enemy
 {
 	namespace StateNode
 	{
+		Normal::Normal(const std::shared_ptr<Enemy::EnemyBase>& owner) :
+			NodeBase_StateMachine(owner)
+		{}
+
+		void Normal::OnStart()
+		{
+
+		}
+
+		bool Normal::OnUpdate()
+		{
+			return true;
+		}
+
+		void Normal::OnExit()
+		{
+
+		}
 
 	}
 }
